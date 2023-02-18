6687e7bed0ac09555534f7e9f046c175c7f379ed
이진 <jean_lee@toss.im>
2023-02-18 20:45:56 +0900

    옹알이 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import level0.OngAlYee;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[]a = {"a","b"};
+
+        new OngAlYee().solution(a);
     }
 }
---
+++ src/level0/OngAlYee.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class OngAlYee {
+
+    //옹알이
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120956
+    public int solution(String[] babbling){
+        int answer = 0;
+        return answer;
+    }
+}
