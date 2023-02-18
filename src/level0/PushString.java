5eafe6d0614ce9aa3ad14d6dab668c106c40c992
jinkyoung <kjink0802@gmail.oom>
2023-02-18 21:22:13 +0900

    2023.02.18 * 문자열밀기, 특이한정렬 껍데기 추가

---
+++ src/level0/PushString.java
@@ -0,0 +1,14 @@
+package level0;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PushString {
+
+    //문자열밀기
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120921
+    public int solution(String A, String B) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/level0/SpecialOrder.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class SpecialOrder {
+
+    //특이한정렬
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120880
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = {};
+        return answer;
+    }
+}
