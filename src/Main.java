01708da88a3e234da04750575832e52e0b62e1c3
jinkyoung <kjink0802@gmail.oom>
2023-02-26 19:30:26 +0900

    2023.02.26 *Level0  - 저주의 숫자 3 템플릿 커밋

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import level0.FuxxNumberThree;
 import level0.OngAlYee;
 import level0.PushString;
 import level0.SpecialOrder;
@@ -5,12 +6,18 @@ import level0.SpecialOrder;
 public class Main {
     public static void main(String[] args) {
 
+        //옹알이
         String[]a = {"a","b"};
         new OngAlYee().solution(a);
 
+        //문자열 밀기
         new PushString().solution("atat","tata");
 
+        //특이한 정렬
         int[]numlist = {10000,20,36,47,40,6,10,7000};
         new SpecialOrder().solution(numlist,30);
+
+        //저주의 숫자 3
+        new FuxxNumberThree().solution(3);
     }
 }
---
+++ src/level0/FuxxNumberThree.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class FuxxNumberThree {
+
+    //저주의 숫자 3
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120871
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
