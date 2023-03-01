ef73b23c8769c96bb7ff28c8fe269078e884f91d
jinkyoung <kjink0802@gmail.oom>
2023-03-01 23:15:47 +0900

    2023.03.01 *Level0  - 다항식 더하기 답변 제출

---
+++ src/level0/AddPolynomial.java
@@ -0,0 +1,27 @@
+package level0;
+
+import java.util.Arrays;
+
+public class AddPolynomial {
+
+    //다항식 더하기
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120863
+    public String solution(String polynomial) {
+        String answer = "";
+        String[] split = polynomial.split(" \\+ ");
+
+        //x개수 합산
+        int xnum = Arrays.stream(split).filter(o -> o.contains("x"))
+                .mapToInt(i -> i.equals("x") ? 1 : Integer.parseInt(i.replace("x","")))
+                .sum();
+        //숫자 합산
+        int num = Arrays.stream(split).filter(o -> !o.contains("x"))
+                .mapToInt(i -> Integer.parseInt(i))
+                .sum();
+
+        answer = xnum == 0 ? "" : xnum == 1 ? "x" : xnum + "x";
+        answer = num == 0 ? answer : answer.equals("") ? Integer.toString(num) : answer + " + " + num;
+
+        return answer;
+    }
+}
