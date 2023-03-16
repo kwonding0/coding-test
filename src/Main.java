8c7e8a6643c86be3d9082ec69b3e509863b78f32
jinkyoung <kjink0802@gmail.oom>
2023-03-16 20:19:51 +0900

    2023.03.16 *Level0  - 안전지대 정답 커밋

--- src/Main.java
+++ src/Main.java
@@ -48,7 +48,10 @@ public class Main {
         int[][] lines = {{-3, -1}, {-2, 1}, {0, 2}};/*{{0, 5}, {3, 9}, {1, 10}};*///{{0, 1}, {2, 3}, {4, 5}};
         new OverlapLineLength().solution(lines);
 
-        int[][] lines2 = {{1, 4}, {9, 2}, {3, 8}, {11, 6}};
-        new Parallel().solution(lines2);
+        int[][] dots = {{1, 4}, {9, 2}, {3, 8}, {11, 6}};
+        new Parallel().solution(dots);
+
+        int[][] board = {{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}};
+        new SafeZone().solution(board);
     }
 }
--- src/level0/SafeZone.java
+++ src/level0/SafeZone.java
@@ -1,13 +1,44 @@
 package level0;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class SafeZone {
 
     //안전지대
     //https://school.programmers.co.kr/learn/courses/30/lessons/120866
-    class Solution {
-        public int solution(int[][] board) {
-            int answer = 0;
-            return answer;
+    public int solution(int[][] board) {
+        int answer = board.length * board.length; //board판 전체 칸 개수
+        Set<List<Integer>> set = new HashSet<List<Integer>>();
+
+        //위험지역 좌표
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    set.add(Arrays.asList(i + 1, j + 1));
+                    set.add(Arrays.asList(i + 1, j));
+                    set.add(Arrays.asList(i + 1, j - 1));
+                    set.add(Arrays.asList(i, j - 1));
+                    set.add(Arrays.asList(i, j));
+                    set.add(Arrays.asList(i, j + 1));
+                    set.add(Arrays.asList(i - 1, j + 1));
+                    set.add(Arrays.asList(i - 1, j));
+                    set.add(Arrays.asList(i - 1, j - 1));
+                }
+            }
         }
+
+        //System.out.println("board = " + Arrays.deepToString(board));
+        //System.out.println("삭제 전set = " + set);
+
+        //board판 벗어나는 위험지역 삭제
+        set.removeIf(list -> list.stream().filter(o -> o < 0 || o >= board.length).collect(Collectors.toList()).size() > 0);
+
+        //System.out.println("삭제 후set = " + set);
+
+        return answer - set.size();
     }
 }
