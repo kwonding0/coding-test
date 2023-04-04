ee41938146791c2eb71bff6594cbff0a185c63c5
jinkyoung <kjink0802@gmail.oom>
2023-04-04 23:50:28 +0900

    2023.04.03 *Level1  - 내적, 약수의 개수와 덧셈 답

--- src/level1/DivisorCntSum.java
+++ src/level1/DivisorCntSum.java
@@ -6,6 +6,19 @@ public class DivisorCntSum {
     //https://school.programmers.co.kr/learn/courses/30/lessons/77884
     public int solution(int left, int right) {
         int answer = 0;
+        int cnt = 0;
+
+        for (int i = left; i <= right; i++) {
+            cnt = 0;
+
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) cnt++;
+                else if (i % j == 0) cnt += 2;
+            }
+
+            answer = cnt % 2 == 0 ? answer + i : answer - i;
+        }
+
         return answer;
     }
 }
--- src/level1/DotProduct.java
+++ src/level1/DotProduct.java
@@ -5,7 +5,12 @@ public class DotProduct {
     //내적
     //https://school.programmers.co.kr/learn/courses/30/lessons/70128
     public int solution(int[] a, int[] b) {
-        int answer = 1234567890;
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
         return answer;
     }
 }
