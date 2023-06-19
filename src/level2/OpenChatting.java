3fd2a3678716845ca8e745c38b2c674aefef74cc
jinkyoung <kjink0802@gmail.oom>
2023-06-19 19:20:05 +0900

    2023.06.19  *Level2   - 주차 요금 계산 템플릿   - 오픈채팅방 템플릿

---
+++ src/level2/OpenChatting.java
@@ -0,0 +1,11 @@
+package level2;
+
+public class OpenChatting {
+
+    //오픈채팅방
+    //https://school.programmers.co.kr/learn/courses/30/lessons/42888
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
---
+++ src/level2/ParkingFee.java
@@ -0,0 +1,11 @@
+package level2;
+
+public class ParkingFee {
+
+    //주차 요금 계산
+    //https://school.programmers.co.kr/learn/courses/30/lessons/92341
+    public int[] solution(int[] fees, String[] records) {
+        int[] answer = {};
+        return answer;
+    }
+}
